8a935408e5573ccec598a1fe2cf0d9ce51b245e8
douglasacademy7 <douglasacademy7@gmail.com>
2018-01-19 15:15:59 -0200

    inicio do projeto, arow functions

---
+++ primeiros-scripts/01.js
@@ -0,0 +1,17 @@
+let i= 10
+i =20
+i = i + 10 /4 * 4
+
+const k = 10
+
+console.log(i)
+
+function soma(num1, num2){
+    return num1 + num2
+}
+
+console.log('soma', soma(10,20))
+
+function sub(num1, num2){
+    return num1 - num2
+}
