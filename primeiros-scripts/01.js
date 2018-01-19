cbf5b1d58dcf68ec5abd0ecf40dbc4a87264ac63
douglasacademy7 <douglasacademy7@gmail.com>
2018-01-19 19:28:00 -0200

    1.0

--- primeiros-scripts/01.js
+++ primeiros-scripts/01.js
@@ -1,15 +1,15 @@
-let i= 10
-i =20
-i = i + 10 /4 * 4
+//let i= 10
+//i =20
+//i = i + 10 /4 * 4
 
-const k = 10
+//const k = 10
 
-console.log(i)
+//console.log(i)
 //----------------------------------------------------------------------------
-//pure function(depende das suas entradas apenas para gerar o retrorno)
+/*pure function(depende das suas entradas apenas para gerar o retrorno)
 function multiplicacao(num1, num2){
     return (num1 * num2)
-}
+}*/
 
 //---------------------------------------------------------------------------
 // arrow functions
@@ -21,5 +21,12 @@ const operacao = (op, num1, num2) => op(num1, num2)
 //console.log('soma', soma(10,20), operacao(sub, 10,30), operacao(soma, 11,11))
 console.log('soma', operacao(soma, 111,111))
 console.log('subtracao', operacao(sub, 100, 50))
-//teste de mais integracao
 console.log('soma de 2 numeros', operacao(soma, 10, 11))
+
+
+/*mapa em java]
+const obj = {
+    key1= 'valor1'
+}
+
+console.log(obj.key1)
