e09c7d252d7b832dfaa11e58177e55d080888acd
douglasacademy7 <douglasacademy7@gmail.com>
2018-01-23 17:31:11 -0200

    High-order-functions-forEach

---
+++ primeiros-scripts/async.js
@@ -0,0 +1,10 @@
+/*console.log('inicio')
+setTimeout(() => {
+    console.log('ola')
+},2000)
+console.log('fim')*/
+
+const arr = [1,2,3,4]
+const print =num=> console.log('Num', num)
+
+arr.forEach(print)
